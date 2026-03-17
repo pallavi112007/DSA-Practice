7128858867f88fbc53ee0ff354730896d159c17a
pallavi112007 <pallavikarre07@gmail.com>
2026-03-17 22:32:03 +0530

    Create Diamond pattern.java

---
+++ Day25/patterns/Diamond pattern.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int n=4;
+		for(int i=0;i<=n;i++)
+		{
+		    int space=n-i;
+		    //to print the spaces 
+		    for(int j=1;j<=space;j++)
+		    {
+		        System.out.print(" ");
+		    }
+		    //to print the starts
+		    for(int j=1;j<=2*i-1;j++)
+		    {
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+		for(int i=n;i>=1;i--)
+		{
+		    int space=n-i;
+		    //to print the spaces 
+		    for(int j=1;j<=space;j++)
+		    {
+		        System.out.print(" ");
+		    }
+		    //to print the starts
+		    for(int j=1;j<=2*i-1;j++)
+		    {
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+
+	}
+}
+// output
+   *
+  ***
+ *****
+*******
+*******
+ *****
+  ***
+   *
