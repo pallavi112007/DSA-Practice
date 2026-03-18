d2f236fd8325b9175991ad81c91d4a9b1cffe8db
pallavi112007 <pallavikarre07@gmail.com>
2026-03-18 22:21:08 +0530

    Create Search Element in Queue.java

---
+++ Day27/Queue/Search Element in Queue.java
@@ -0,0 +1,102 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class QueueUsingArray
+{
+        int rear,front;
+        int arr[];
+        int capacity;
+    QueueUsingArray(int size)
+    {
+        capacity=size;
+        arr=new int[size];
+        rear=-1;front=0;
+    }
+    void enqueue(int data){
+        if(rear==capacity-1)
+        {
+            System.out.print("Queue is fulled bro");
+            return;
+        }
+        arr[++rear]=data;
+    }
+    int dequeue()
+    {
+        if(front>rear)
+        {
+            System.out.print("Queue is Empty");
+            return -1;
+        }
+        return arr[front++];
+    }
+    int peek()
+    {
+        if(front>rear)
+        {
+            System.out.print("Queue is empty");
+            return -1;
+        }
+        return arr[front];
+    }
+    int count=0;
+    void display()
+    {
+        if(front>rear)
+        {
+            System.out.print("Queue is empty");
+        }
+        else
+        {
+            for(int i=front;i<=rear;i++)
+            {
+                System.out.print(arr[i]+" ");
+            }
+        }
+        System.out.println();
+    }
+    int size()
+    {
+        return (rear-front+1);
+    }
+    boolean isEmpty()
+    {
+        return front > rear;
+    }
+    int getsum()
+    {
+        int sum=0;
+        for(int i=front;i<=rear;i++)
+            {
+                sum+=arr[i];
+            }
+            return sum;
+    }
+    boolean search(int element)
+    {
+        for(int i=front;i<=rear;i++)
+        {
+            if(element==arr[i])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    QueueUsingArray q=new QueueUsingArray(5);
+	    q.enqueue(1);
+	    q.enqueue(2);
+	    q.enqueue(3);
+	    q.enqueue(1);
+	    q.enqueue(2);
+	    q.display();
+	    System.out.println("sum of the queue is: "+q.getsum());
+	    System.out.println("element is there : "+q.search(3));
+	}
+}
+//output
+1 2 3 1 2 
+sum of the queue is: 9
+sum of the queue is: true
