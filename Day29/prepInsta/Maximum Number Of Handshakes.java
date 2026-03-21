e1c65e5e358af1fafbd1578448a9dd5dc415029e
pallavi112007 <pallavikarre07@gmail.com>
2026-03-21 20:58:58 +0530

    Create Maximum Number Of Handshakes.java

---
+++ Day29/prepInsta/Maximum Number Of Handshakes.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int m=n*(n-1)/2;
+	    System.out.println(m);
+	}
+}
+//output
+45
