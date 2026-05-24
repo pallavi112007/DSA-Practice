4e49f5fd26e661e0a8492212c6ef147b112e89e5
pallavi112007 <pallavikarre07@gmail.com>
2026-05-24 17:30:17 +0530

    Create Find ceil of a number.java

---
+++ Day37/Binary search/Find ceil of a number.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int arr[]={10,20,30,40,50};
+        int target=35;
+        int left=0,right=arr.length-1;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(arr[mid]==target)
+            {
+                System.out.println(arr[mid]);
+                return;
+            }
+            else if(arr[mid]<target)
+            {
+                left=mid+1;
+            }
+            else
+            {
+                right=mid-1;
+            }
+        }
+        if(left<arr.length)
+        {
+            System.out.println("nearst greater value of target is: "+arr[left]);
+        }
+        else
+        {
+            System.out.println("-1");
+        }
+    }
+}
+//output
+40
