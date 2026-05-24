5d62ccb2d56826b1fff1ac174f4315d162d6a8b2
pallavi112007 <pallavikarre07@gmail.com>
2026-05-24 12:26:22 +0530

    Create Find first occurrence of an element.java

---
+++ Day37/Binary search/Find first occurrence of an element.java
@@ -0,0 +1,38 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code onl
+// Online Java Compiler
+import java.util.*;
+class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int arr[]={10,20,20,20,20,20,50};
+        int left=0,right=arr.length-1;
+        int target=20;
+        int ans=0;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(arr[mid]==target)
+            {
+                ans=mid;
+                right=mid-1;
+            }
+            else if(target>arr[mid])
+            {
+                left=mid+1;
+            }
+            else
+            {
+                right=mid-1;
+            }
+        }
+        System.out.print(ans);
+    }
+}
+//output
+1
