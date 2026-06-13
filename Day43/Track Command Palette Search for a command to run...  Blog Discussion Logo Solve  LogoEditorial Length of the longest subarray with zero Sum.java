02908f717d2164513034644665a8104b4499e1b9
pallavi112007 <pallavikarre07@gmail.com>
2026-06-13 23:15:01 +0530

    Create  Track Command Palette Search for a command to run...  Blog Discussion Logo Solve  LogoEditorial Length of the longest subarray with zero Sum.java

---
+++ Day43/Track Command Palette Search for a command to run...  Blog Discussion Logo Solve  LogoEditorial Length of the longest subarray with zero Sum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        int arr[] = {10, 5, 2, 7, 1, 9};
+
+        int max = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+
+                if (sum == 0) {
+                    max = Math.max(max, j - i + 1);
+                }
+            }
+        }
+
+        System.out.print(max);
+    }
+}
