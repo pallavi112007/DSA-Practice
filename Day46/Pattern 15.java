86bfc309453a3b63926961ab4cfeb52157e062c4
pallavi112007 <pallavikarre07@gmail.com>
2026-06-17 20:04:05 +0530

    Create Pattern 15.java

---
+++ Day46/Pattern 15.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static void main(String[] args) {
+    int n = 5;
+    for (int i = 5; i >= 0; i--) 
+    {
+        for (char j = 'A'; j <= 'A'+i; j++) 
+        {
+            System.out.print(j);
+        }
+        System.out.println();
+        }
+    }
+}
+//output
+ABCDEF
+ABCDE
+ABCD
+ABC
+AB
+A
