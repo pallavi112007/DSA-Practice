b0e5fdbd5ac7fdd07074fb4c96fb684cb87affee
pallavi112007 <pallavikarre07@gmail.com>
2026-06-17 19:32:27 +0530

    Create striver-patter-8.java

---
+++ Day46/striver-patter-8.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String[] args)
+    {
+        int n=6;
+        for(int i=5;i>=1;i--)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++)
+            {
+                System.out.print("*");
+            }
+             System.out.println();
+        }
+    }
+}
+//output
+*********
+ *******
+  *****
+   ***
+    *
