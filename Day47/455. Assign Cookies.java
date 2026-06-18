74fcd9764fe962a17677512b0c18b22938399373
pallavi112007 <pallavikarre07@gmail.com>
2026-06-18 23:33:34 +0530

    Create 455. Assign Cookies.java

---
+++ Day47/455. Assign Cookies.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int l=0;
+        int r=0;
+        int n=g.length;int m=s.length;
+        while(l<n && r<m)
+        {
+            if(g[l]<=s[r])
+            {
+                r++;
+                l++;
+            }
+            else{
+            r++;
+            }
+        }
+        return l;
+    }
+}
