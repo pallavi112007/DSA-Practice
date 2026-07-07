1c6629b72f100812e8d214787c89f8357feb8f54
pallavi112007 <pallavikarre07@gmail.com>
2026-07-08 00:06:14 +0530

    Create fibanicci.java

---
+++ Day61/recursion/fibanicci.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution{
+    public static int print(int n)
+    {
+        if(n==0)
+        {
+            return 0;
+        }
+        if(n==1)
+        {
+            return 1;
+        }
+        return print(n-1)+print(n-2);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int n=sc.nextInt();
+        int result=print(n);
+        System.out.println(result);
+    }
+}
+//output
+Enter a number:3
+2
