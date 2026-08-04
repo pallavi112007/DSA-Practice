a60260eae321b5ad3ad82dbfcd95c9103044fe75
pallavi112007 <pallavikarre07@gmail.com>
2026-08-04 23:43:50 +0530

    Create 438. Find All Anagrams in a String.java

---
+++ Day71/438. Find All Anagrams in a String.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> list=new ArrayList<>();
+        if (p.length() > s.length()) {
+            return list;
+        }
+         int pfreq[]=new int[26];
+         int sfreq[]=new int[26];
+        for(int i=0;i<p.length();i++)
+        {
+            pfreq[p.charAt(i)-'a']++;
+            sfreq[s.charAt(i)-'a']++;
+        }
+        if(Arrays.equals(pfreq,sfreq))
+        {
+            list.add(0);
+        }
+        for(int i=p.length();i<s.length();i++)
+        {
+            sfreq[s.charAt(i)-'a']++;
+            sfreq[s.charAt(i-p.length())-'a']--;
+            if(Arrays.equals(pfreq,sfreq))
+            {
+                list.add(i-p.length()+1);
+            }
+        }
+        return list;
+    }
+}
