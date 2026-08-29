e1e811f26480448f76171b4d0e8417b96994cfe1
pallavi112007 <pallavikarre07@gmail.com>
2026-08-30 01:12:22 +0530

    Create 1342. Number of Steps to Reduce a Number to Zero.java

---
+++ Day77/1342. Number of Steps to Reduce a Number to Zero.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        int count=0;
+        while(num>0)
+        {
+            count++;
+            if(num%2==0)
+            {
+                num=num/2;
+            }
+            else
+            {
+                num-=1;
+            }
+        }
+        return count;
+    }
+}
